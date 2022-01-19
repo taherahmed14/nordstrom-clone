5190fbbe0f3bdd49b8439a46448dd9793588c7ea
Shakib40 <shakibjilani@gmail.com>
2022-01-19 12:52:18 +0530

    Process.txt
    For, what package I installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { SendOTP } from './components/Email/Form';
 import { Login } from './components/Login';
 import { Register } from "./components/Register";
-import {Products} from "./components/Products/Products";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="login" element={<Login />}></Route>
         <Route path="register" element={<Register />}></Route>
         <Route path="/verification" element={<SendOTP />}></Route>
-        <Route path ="/products" element ={<Product />}> </Route>
+        <Route path ="/products" element ={<Products/>}> </Route>
       </Routes>
     </div>
   );
