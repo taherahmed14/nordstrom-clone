d3f6d23052e22a6db12cf356c53ddc413ef5b92f
Shakib40 <shakibjilani@gmail.com>
2022-01-19 19:25:27 +0530

    trying to connet product page to product details

---
+++ Frontend/src/Features/Products/reducer.js
@@ -0,0 +1,80 @@
+import { 
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    ALL_PRODUCT_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    CLEAR_ERROR
+} 
+from "./actionTypes"
+
+const initialState = { products: [], token: "", setName: "" , productsCount: 0};
+
+export const productReducer = ( state = initialState, {type,payload}) =>{
+     
+    switch(type){
+        case ALL_PRODUCT_REQUEST:
+        return{
+                loading : true,
+                products : []
+        }
+        case ALL_PRODUCT_SUCCESS : 
+        return{
+             
+              loading : false,
+              products : payload,
+           
+        }
+        case ALL_PRODUCT_FAIL : 
+        return{
+             loading : false,
+             error : payload,
+        }
+        case CLEAR_ERROR : 
+        return{
+             ...state,
+             error : null,
+        }
+
+        default: 
+         return{
+            state
+         }
+
+    }
+}
+
+
+export const productDetailsReducer = ( state = {products:{} }, {type,payload}) =>{
+     
+    switch(type){
+        case PRODUCT_DETAILS_REQUEST:
+        return{
+                loading : true,
+                ...state,
+        }
+        case PRODUCT_DETAILS_SUCCESS : 
+        return{
+             
+              loading : false,
+              products : payload,
+        }
+        case PRODUCT_DETAILS_FAIL : 
+        return{
+             loading : false,
+             error : payload,
+        }
+        case CLEAR_ERROR : 
+        return{
+             ...state,
+             error : null,
+        }
+
+        default: 
+         return{
+            state
+         }
+
+    }
+}
