8b307649e7c76efef3d4dfa1b2df6def37975beb
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-01-21 01:03:43 +0530

    product detail done on click cart button posting data

--- Frontend/src/components/Product/ProductDetails.jsx
+++ Frontend/src/components/Product/ProductDetails.jsx
@@ -1,60 +1,90 @@
-import React from 'react';
+import React from 'react'
 import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { productsDetailsLoading, productsDetailsSuccess, productsDetailsFail } from '../../Features/Product/action';
-import { useParams } from 'react-router-dom';
-import Loader from '../Loader/Loader';
-
-
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  productsDetailsLoading,
+  productsDetailsSuccess,
+  productsDetailsFail,
+} from '../../Features/Product/action'
+import { useParams } from 'react-router-dom'
+import Loader from '../Loader/Loader'
+import ProductInfo from '../ProductDetailPage/ProductInfo'
+import styles from '../ProductDetailPage/Product.module.css'
+import ProductDetail from '../ProductDetailPage/ProductDetail'
 export const ProductDetails = () => {
+  const dispatch = useDispatch()
+  const { id } = useParams()
+  const [details, setDetails] = useState({})
 
-    const dispatch = useDispatch();
-    const { id } = useParams();
-    const [details, setDetails] = useState({});
+  useEffect(() => {
+    getData()
+  }, [dispatch, id])
 
-    useEffect(() => {
-        getData()
-    }, [dispatch, id])
+  const getData = () => {
+    dispatch(productsDetailsLoading())
+    fetch(`http://localhost:4500/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(productsDetailsSuccess(data))
+        setDetails(data)
+      })
+      .catch((err) => {
+        dispatch(productsDetailsFail(err))
+      })
+  }
+  const { loading } = useSelector((state) => state.productsDetailState)
+  console.log('Loading', loading)
 
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className={styles.bigcontainer}>
+            <div
+              style={{
+                display: 'flex',
+                gap: '2%',
+                marginTop: '2%',
 
-    const getData = () => {
-        dispatch(productsDetailsLoading());
-        fetch(`http://localhost:4500/products/${id}`)
-            .then((response) => response.json())
-            .then((data) => {
-                dispatch(productsDetailsSuccess(data))
-                setDetails(data);
-            })
-            .catch((err) => {
-                dispatch(productsDetailsFail(err));
-            });
-    };
-    const { loading } = useSelector((state) => state.productsDetailState)
-    console.log("Loading", loading);
-
-    return <div>
-        {loading ? (
-            <Loader />
-        ) : (
-            <div>
-                <span>ProductDetails</span>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>{details.name}</td>
-                            <td>{details.description}</td>
-                            <td>{details.price}</td>
-                            {details.images && details.images.map((image) => (
-                                <td>
-                                    <img src={image} alt={details.name}></img>
-                                </td>
-
-                            ))}
-
-                        </tr>
-                    </tbody>
-                </table>
+                width: '61%',
+                flexWrap: 'wrap',
+                marginLeft: '1%',
+              }}
+            >
+              {details.images &&
+                details.images.map((image) => (
+                  <div
+                    style={{
+                      width: 380,
+                      cursor: 'crosshair',
+                      margin: '2%',
+                    }}
+                  >
+                    <img
+                      src={image}
+                      alt={details.name}
+                      height="690px"
+                      width="380px"
+                    ></img>
+                  </div>
+                ))}
             </div>
-        )}
-    </div>;
-};
+
+            <ProductDetail
+              name={details.name}
+              brand={details.brand}
+              category={details.category}
+              description={details.description}
+              price={details.price}
+              image={details.images}
+            />
+          </div>
+
+          <ProductInfo />
+        </>
+      )}
+    </div>
+  )
+}
