02f4fa31154194498ec1eb855480ef0360098917
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-01-21 03:24:02 +0530

    formating the products of productlist page

--- Frontend/src/components/Product/ProductCard.jsx
+++ Frontend/src/components/Product/ProductCard.jsx
@@ -1,32 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 // import { Rating } from "@material-ui/lab";
-import ReactStars from "react-rating-stars-component";
+import styles from './Product.module.css'
+import ReactStars from 'react-rating-stars-component'
 export const ProductCard = ({ product }) => {
+  const options = {
+    edit: false,
+    color: 'rgba(20,20,20,0.2)',
+    activeColor: 'tomato',
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  }
+  return (
+    <table className="ProductCard">
+      <tbody>
+        <tr>
+          <td>
+            <Link to={`/products/${product._id} `}>{product.name}</Link>
+          </td>
+          <td>{product.price}</td>
+          <td>{product.category}</td>
+          <td>{product.stock}</td>
+          <td>{product.rating}</td>
+          <td>
+            {' '}
+            <ReactStars {...options} />{' '}
+          </td>
+          <td>{product.numOfReviews}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
-    const options = {
-        edit: false,
-        color: "rgba(20,20,20,0.2)",
-        activeColor: "tomato",
-        size: window.innerWidth < 600 ? 20 : 25,
-        value: product.ratings,
-        isHalf: true,
-    };
-    return (
-        <table className="ProductCard" >
-            <tbody>
-                <tr>
-                    <td><Link to={`/products/${product._id} `} >{product.name}</Link></td>
-                    <td>{product.price}</td>
-                    <td>{product.category}</td>
-                    <td>{product.stock}</td>
-                    <td>{product.rating}</td>
-                    <td> <ReactStars {...options} /> {" "}</td>
-                    <td>{product.numOfReviews}</td>
-                </tr>
+// return (
+//     <div>
+//       {loading ? (
+//         <Loader />
+//       ) : (
+//         <div className={styles.bigcontainer}>
+//           <div className={styles.leftw}></div>
 
-            </tbody>
-        </table>
-    );
-};
+//           <div className={styles.rightw}>
+//             {products &&
+//               products.map((product, idx) => (
+//                 <div key={idx} className={styles.prod}>
+//                   {/* <Link to={`/products/${product._id} `}>{product.name}</Link> */}
+//                   <img
+//                     src={product.images[0]}
+//                     alt=""
+//                     className={styles.image}
+//                   />
+//                 </div>
+//               ))}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   )
 
+// grid formating of product with right dimension
--- Frontend/src/components/Product/Products.jsx
+++ Frontend/src/components/Product/Products.jsx
@@ -1,133 +1,143 @@
-import React from 'react';
-import "./Products.css";
-import { ProductCard } from './ProductCard';
+import React from 'react'
+import './Products.css'
+import { ProductCard } from './ProductCard'
+import styles from './Product.module.css'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import ReactStars from 'react-rating-stars-component'
+import Loader from '../Loader/Loader'
+import Typography from '@material-ui/core/Typography'
+import Slider from '@material-ui/core/Slider'
+import { useParams } from 'react-router-dom'
+import Pagination from 'react-js-pagination'
 
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ReactStars from "react-rating-stars-component";
-import Loader from '../Loader/Loader';
-import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
-import { useParams } from 'react-router-dom';
-import Pagination from "react-js-pagination";
-
-
-import { getproductsLoading, getproductsSuccess, productsDetailsFail } from '../../Features/Product/action';
+import {
+  getproductsLoading,
+  getproductsSuccess,
+  productsDetailsFail,
+} from '../../Features/Product/action'
 
 const categories = [
-    "Accessories",
-    "Beauty %26 Grooming",
-    "Clothing",
-    "Home",
-    "Shoes"
-];
-
+  'Accessories',
+  'Beauty %26 Grooming',
+  'Clothing',
+  'Home',
+  'Shoes',
+]
 
 export const Products = () => {
+  const dispatch = useDispatch()
+  const [products, setProducts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [price, setPrice] = useState([0, 4000])
+  const [category, setCategory] = useState('')
+  // const [ratings, setRatings] = useState(0);
+  // const [keywords, setKeywords] = useState("");
 
-    const dispatch = useDispatch();
-    const [products, setProducts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [price, setPrice] = useState([0, 4000]);
-    const [category, setCategory] = useState("");
-    // const [ratings, setRatings] = useState(0);
-    // const [keywords, setKeywords] = useState("");
+  const {
+    loading,
+    filteredProductsCount,
+    resultPerPage,
+    productsCount,
+  } = useSelector((state) => state.productsState)
 
-    const { loading, filteredProductsCount, resultPerPage, productsCount } = useSelector((state) => state.productsState)
+  // const { keyword } = useParams();
+  // console.log("keyword", keyword);
 
-    // const { keyword } = useParams();
-    // console.log("keyword", keyword);
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice)
+  }
 
-    const priceHandler = (event, newPrice) => { setPrice(newPrice); };
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e)
+  }
 
-    const setCurrentPageNo = (e) => { setCurrentPage(e); };
+  const getData = () => {
+    dispatch(getproductsLoading())
+    let link = `http://localhost:4500/products?page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`
 
+    if (category) {
+      link = `http://localhost:4500/products?keyword=${keywords}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}`
+    }
 
-    const getData = () => {
-        dispatch(getproductsLoading());
-        let link = `http://localhost:4500/products?page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`;
+    fetch(link)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(getproductsSuccess(data))
+        setProducts(data.products)
+      })
+      .catch((err) => {
+        dispatch(productsDetailsFail(err))
+      })
+  }
 
-        if (category) {
-            link = `http://localhost:4500/products?keyword=${keywords}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}`;
-        }
+  let count = filteredProductsCount
 
-        fetch(link)
-            .then((response) => response.json())
-            .then((data) => {
-                dispatch(getproductsSuccess(data))
-                setProducts(data.products);
-            })
-            .catch((err) => {
-                dispatch(productsDetailsFail(err));
-            });
-    };
+  useEffect(() => {
+    getData()
+  }, [dispatch, keyword, currentPage, price, category])
 
-    let count = filteredProductsCount;
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h1>Products ({count})</h1>
 
-    useEffect(() => {
-        getData()
-    }, [dispatch, keyword, currentPage, price, category])
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={1500}
+              max={3000}
+            />
 
-    return <div>
-        {loading ? (
-            <Loader />
-        ) : (
-            <div>
-                <h1>Products ({count})</h1>
+            <Typography>Product Type</Typography>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                <div className="filterBox">
-                    <Typography>Price</Typography>
-                    <Slider
-                        value={price}
-                        onChange={priceHandler}
-                        valueLabelDisplay="auto"
-                        aria-labelledby="range-slider"
-                        min={1500}
-                        max={3000}
-                    />
+          <div>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
 
-                    <Typography>Product Type</Typography>
-                    <ul className="categoryBox">
-                        {categories.map((category) => (
-                            <li
-                                className="category-link"
-                                key={category}
-                                onClick={() => setCategory(category)}
-                            >
-                                {category}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div>
-                    {products &&
-                        products.map((product) => (
-                            <ProductCard key={product._id} product={product} />
-                        ))}
-                </div>
-
-                {resultPerPage < productsCount && (
-                    <div className="paginationBox">
-                        <Pagination
-                            activePage={currentPage}
-                            itemsCountPerPage={resultPerPage}
-                            totalItemsCount={productsCount}
-                            onChange={setCurrentPageNo}
-                            nextPageText="Next"
-                            prevPageText="Prev"
-                            firstPageText="1st"
-                            lastPageText="Last"
-                            itemClass="page-item"
-                            linkClass="page-link"
-                            activeClass="pageItemActive"
-                            activeLinkClass="pageLinkActive"
-                        />
-                    </div>
-                )}
+          {resultPerPage < productsCount && (
+            <div className="paginationBox">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
             </div>
-        )}
-    </div>;
-};
-
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
