84674829c41680cc6ce308a0958a983c9c885b56
vivek <vivekkumarvky13@gmail.com>
2022-01-21 18:05:53 +0530

    Pages
    Cart and Checkout Payment page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,6 +8,9 @@ import { OTPpage } from './components/OTP/OTPpage';
 import { OrderConfirm } from './components/OrderConfirm/Acknowldegment';
 import {Products} from './components/Product/Products';
 import {ProductDetails} from './components/Product/ProductDetails';
+import { Checkout } from "./components/Checkoutandpayment/Checkout";
+import { Payment } from "./components/Checkoutandpayment/Payment";
+import { ProductCart } from "./components/ProductCart/ProductCart";
 
 function App() {
   return (
@@ -20,18 +23,21 @@ function App() {
         <Route path="/verification" element={<SendOTP />}></Route>
         <Route path="/OTP" element={<OTPpage />}></Route>
         <Route path="/orderConfirm" element={<OrderConfirm />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/payment" element={<Payment />}></Route>
+        <Route path="/cart" element={<ProductCart />}></Route>
 
         <Route exact path="/products" element={<Products />}>
-          {' '}
+          {" "}
         </Route>
         <Route path="/products/:keyword" component={Products} />
         <Route exact path="/product/:id" element={<ProductDetails />}>
-          {' '}
+          {" "}
         </Route>
       </Routes>
       {/* <Footer /> */}
     </div>
-  )
+  );
 }
 
 export default App
