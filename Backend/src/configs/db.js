b4ba6659669c27b2c041b8266f59653f93454968
Shakib40 <shakibjilani@gmail.com>
2022-01-22 18:48:25 +0530

    added .env file in backend, added one more product in db , also added rating

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config();
+
+// module.exports = () => {
+//   return mongoose.connect(
+//     `mongodb+srv://Gaurav16:gaurav16@cluster0.5rsgd.mongodb.net/Nordstorm`,
+//   )
+// }
+
+
 module.exports = () => {
   return mongoose.connect(
-    `mongodb+srv://Gaurav16:gaurav16@cluster0.5rsgd.mongodb.net/Nordstorm`,
+    `mongodb+srv://Gaurav16:${process.env.DB_PASS}@cluster0.5rsgd.mongodb.net/Nordstorm`,
   )
 }
