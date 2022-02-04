d3e732b360bf20011758498d3819e4b9bb0b7607
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-02-04 18:18:56 +0530

    development processing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,13 +1,41 @@
-const connect = require('./configs/db')
+const connect = require('./configs/db');
 
-const app = require('./index')
+const express = require("express");
 
+const app = require('./index');
+
+const path = require("path");
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:4500', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+//app.use(cors(corsOptions))
 
 const port = process.env.PORT || 4500;
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'Frontend/build'))); //check
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
+    });
+  }
+
 
 app.listen(port, async (req, res) => {
-    await connect()
+    await connect();
 
     console.log(`Listening on PORT ${port}`)
 })
