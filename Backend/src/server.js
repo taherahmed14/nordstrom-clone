9130be6cf2f66fbe5d2785eb1eea1f86ec0170d0
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-02-04 22:46:33 +0530

    dep

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, '../Frontend/build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../Frontend/build/index.html'))
+  res.sendFile(path.join(__dirname + '../Frontend/build/index.html'))
 })
 
 // if (process.env.NODE_ENV === 'production') {
